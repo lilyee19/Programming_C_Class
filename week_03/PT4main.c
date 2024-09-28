5128edeea04df949023dc349b7a51cf26fc64e8c
lilyee19 <banny19@naver.com>
2024-09-28 16:18:35 +0900

    PT4

---
+++ week_03/PT4main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(main) {
+	
+	float a;
+	float b;
+	
+	printf("enter two integers : ");
+	scanf("%f, %f", &a, &b);
+	
+	printf("%f / %f = %f\n", a,b, a/b);
+	
+	return 0;
+}
+
